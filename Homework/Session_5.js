c96c166c3298af9f52d4c5e639ee7b2c3d35ddf1
NamAnh0920 <namanhnguyenx@gmail.com>
2020-10-13 20:32:49 +0700

    initial commit

--- Homework/Session_5.js
+++ Homework/Session_5.js
@@ -1,17 +1,7 @@
 let n = parseInt(prompt('Enter a number'));
 let arr = [];
 for (i = 0; i < n; i++) {
-    arr.push(Math.random());
+    arr.push(Math.random()); 
 }
-let cnt = 0;
-while (n > 0) {
-    n = n%10;
-    cnt ++;
-}
-while (true) {
-    num = Math.round(Math.random()*(10**cnt));
-    if (num < arr.length) {
-        console.log(`${num}. ${arr[num]}`);
-        break;
-    }    
-}
+let num = Math.round(Math.random()*arr.length);
+console.log(`${num}. ${arr[num]}`);
